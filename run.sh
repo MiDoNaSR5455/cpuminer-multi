08918ebe7856c23110a36102260b4f97eed9b46e
MiDoNaSR5455 <77590671+MiDoNaSR5455@users.noreply.github.com>
2021-02-22 14:24:05 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./cpuminer -a sha256d -o stratum+tcp://connect.pool.bitcoin.com:3333 -u DSUmeYKGd2UJja43MsnAhA2kHaQoBiafwb -p c=DOGE
+./cpuminer -a sha256d -o stratum+tcp://connect.pool.bitcoin.com:3333 -u D9ThT7LkhyU2dd3u1KSo633sAgXHsHf9zh -p c=DOGE
